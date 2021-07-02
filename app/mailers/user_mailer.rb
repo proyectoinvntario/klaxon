bff590b872c2bd2c2dc8f3d4d0f971e9d3d30413
rafageist <rafageist@hotmail.com>
2021-07-01 21:49:10 -0300

    update

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -18,7 +18,7 @@ class UserMailer < ApplicationMailer
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
     response = sg.client.mail._('send').post(request_body: mail.to_json)
 
-    mail(to: @user.email, subject: 'Log in to Klaxon')
+    # mail(to: @user.email, subject: 'Log in to Klaxon')
   end
 
   def welcome_email(user: nil, invited_by: nil)
